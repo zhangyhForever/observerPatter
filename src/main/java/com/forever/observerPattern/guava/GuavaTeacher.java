92c90668ba7c62bc100b2e5a333fa26a32a6a79e
zhangyongheng <zhangyongheng@zzjinhui.com>
2019-04-19 17:15:12 +0800

    guava

--- src/main/java/com/forever/observerPattern/guava/GuavaTeacher.java
+++ src/main/java/com/forever/observerPattern/guava/GuavaTeacher.java
@@ -17,7 +17,7 @@ public class GuavaTeacher {
 
     @Subscribe
     public void notice(GuavaGPer gPer){
-        System.out.println(name+"老师，你好！您的学生在"+gPer.getName()+"上收到信息:\n" +
-                "消息的内容是："+gPer.getQuestion().getContent()+"\n来自："+gPer.getQuestion().getAskerName());
+        System.out.println(name+"老师，你好！您的学生在"+gPer.getName()+"提出了问题:\n" +
+                "内容："+gPer.getQuestion().getContent()+"\n来自："+gPer.getQuestion().getAskerName());
     }
 }
